7ba601e07d54e4efa60244a1cc69868f5b49b4ef
Destin <dvhunt7@gmail.com>
2017-12-12 11:38:44 -0500

    it works!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,31 +10,43 @@ var computerChoices = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j"
 
 document.onkeyup = function(event) {
 
-	var userGuess = event.key;
-
-	var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+	guesses.push(userGuess);
 
 	if  (computerGuess === userGuess) {
 		
 			wins++;
+			alert("You win!");
+			guessesleft = 9;
+			guesses = [];
 
 		}
 
 
 	else {
 
+		guessesleft--;
+	}
+
+	if (guessesleft == 0){
+
 		losses++;
+		alert("You Lose :(");
+		guessesleft = 9;
+		guesses = [];
+		
 	}
 
 
 
 var html = 
 		"<p>Wins: " + wins + "</p>" +
-		"<p>Losses: " + losses + "</p>"
-		"<p> Guesses Left: " + guessesleft + "</p>"
-		"<p> Guesses so far: " + guesses + "</p>";
+		"<p>Losses: " + losses + "</p>" +
+		"<p>Guesses Left: " + guessesleft + "</p>" +
+		"<p>Guesses so far: " + guesses + "</p>";
 
 
 document.querySelector("#stats").innerHTML = html;
